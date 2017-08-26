48540c88072852609c0f4c2a39c0138de3d6f37d
Geoff Mayes <mayesgr@gmail.com>
2017-08-25 22:40:38 -0700

    * Add Angular source into gh-pages-source/ directory. * Add the built Angular files to the root directory.

---
+++ gh-pages-source/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent {
+}
